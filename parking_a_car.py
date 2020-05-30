ddbc46f13429f5f935612152a89e1df44ee524df
Berger <Daniel.Berger@L-P-A.com>
2020-05-30 19:06:56 +0200

    using levenberg marquardt optimizer for solving the lagrangian system of equations

--- parking_a_car.py
+++ parking_a_car.py
@@ -33,21 +33,21 @@ class ParkingCar:
         self._q_t[0] = np.array([q_1_0, q_2_0])
         self._q_dot_t[0] = np.array([q_dot_1_0, q_dot_2_0])
         # determine q_t[1] from q_dot[0] and q_t[0]
-        self._q_t[1] = self._q_0 + self._dt * self._q_dot_t[0]
+        self._q_t[1] = self._q_t[0] + self._dt * self._q_dot_t[0]
 
-        self._phi_t = np.zeros((nbSteps, 2))
-        self._phi_dot_t = np.zeros((nbSteps-1, 2))
+        self._phi_t = np.zeros((nbSteps))
+        self._phi_dot_t = np.zeros((nbSteps-1))
         self._phi_t[0] = phi_0
         # find phi_dot[0] from 0 = M(q, q_dot, phi, phi_dot)
-        self._phi_dot_t[0] = - self._q_dot_1 * math.cos(self._phi_0) + self._q_dot_2 * math.sin(self._phi_0)
-        self._phi_t[1] = self._phi_0 + self._dt * self._phi_dot_t[0]
+        self._phi_dot_t[0] = - self._q_dot_t[0][0] * math.cos(self._phi_t[0]) + self._q_dot_t[0][1] * math.sin(self._phi_t[0])
+        self._phi_t[1] = self._phi_t[0] + self._dt * self._phi_dot_t[0]
 
         self._lambda_t = np.zeros((nbSteps-1))
         self._lambda_t[0] = lambda_0
 
-        self._p_t = np.zeros((nbSteps))
+        self._p_t = np.zeros((nbSteps,2))
         # determine p_t[0] 
-        self._p_t[0] = self._q_t[0] + np.array([math.sin(self._phi_0), math.cos(self._phi_0)])
+        self._p_t[0] = self._q_t[0] + np.array([math.sin(self._phi_t[0]), math.cos(self._phi_t[0])])
 
     def _L(self, i):
         return math.sqrt(2.0 * self._q_dot_t[i][0] * self._q_dot_t[i][0] \
@@ -74,9 +74,10 @@ class ParkingCar:
         return - 2.0 * self._phi_dot_t[i] / self._L(i) + self._lambda_t[i]
 
 
-    def _Lagrange(self, i, q_t_i, lambda_t_i_minus_one):
+    def _Lagrange(self, i, q_t_i_1, q_t_i_2, lambda_t_i_minus_one):
         # determine q_t_i_minus_one
-        self._q_dot_t[i-1] = (q_t_i - self._q_t[i-1]) / self._dt
+        self._q_dot_t[i-1][0] = (q_t_i_1 - self._q_t[i-1][0]) / self._dt
+        self._q_dot_t[i-1][1] = (q_t_i_2 - self._q_t[i-1][1]) / self._dt
 
         # determine phi_t_i_minus_one using M = 0
         self._phi_dot_t[i-1] = - self._q_t[i-1][0] * math.cos(self._phi_t[i-1]) + self._q_t[i-1][1] * math.sin(self._phi_t[i-1])
@@ -88,22 +89,29 @@ class ParkingCar:
         self._lambda_t[i-1] = lambda_t_i_minus_one
 
         # Lagrange equation for q_1
-        lagrange_q_1_i = - (self._dG_dq_dot_1[i-1] - self._dG_dq_dot_1[i-2]) / self._dt
+        lagrange_q_1_i = - (self._dG_dq_dot_1(i-1) - self._dG_dq_dot_1(i-2)) / self._dt
 
         # Lagrange equation for q_2
-        lagrange_q_2_i = - (self._dG_dq_dot_2[i-1] - self._dG_dq_dot_2[i-2]) / self._dt  
+        lagrange_q_2_i = - (self._dG_dq_dot_2(i-1) - self._dG_dq_dot_2(i-2)) / self._dt  
 
         # Lagrange equation for phi
         lagrange_phi_i = self._dG_dphi(i) - (self._dG_dphi_dot(i-1) - self._dG_dphi_dot(i-2)) / self._dt
 
         return np.array([lagrange_q_1_i, lagrange_q_2_i, lagrange_phi_i])
 
-    def solve(self):
-        for t_i in range(2, self._length):
+    def park(self):
+        for i in range(2, self._nbSteps):
             # initialize start values
 
-            x_0 = [self._q_t[i-1][0], self._q_t[i-1][1], self._phi_t[i-2]]
-            op.root(_Lagrange, x0, method='lm', jac=None, tol=None, callback=None, options=None)
+            x_0 = [self._q_t[i-1][0], self._q_t[i-1][1], self._lambda_t[i-2]]
+
+            def _Lagrange_(x):
+                q_t_i_1 = x[0]
+                q_t_i_2 = x[1]
+                lambda_t_i_minus_one = x[2]
+                return self._Lagrange(i, q_t_i_1, q_t_i_2, lambda_t_i_minus_one)
+
+            op.root(_Lagrange_, x_0, method='lm', jac=None, tol=None, callback=None, options=None)
 
 
 
---
+++ program.py
@@ -0,0 +1,11 @@
+from parking_a_car import *
+
+q_1_0 = 1.0
+q_2_0 = 0.0
+q_dot_1_0 = 1.0
+q_dot_2_0 = 0.0
+phi_0 = 0.0
+lambda_0 = 1.0
+
+parkingCar = ParkingCar(q_1_0, q_2_0, q_dot_1_0, q_dot_2_0, phi_0, lambda_0)
+parkingCar.park()
